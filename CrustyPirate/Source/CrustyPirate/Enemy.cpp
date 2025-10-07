e2e6bfb93700284939e067fee4c5c1f15ac40965
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-10-07 12:25:54 +0700

    update "Enemy follow Player"

--- CrustyPirate/Source/CrustyPirate/Enemy.cpp
+++ CrustyPirate/Source/CrustyPirate/Enemy.cpp
@@ -3,6 +3,7 @@
 
 #include "Enemy.h"
 
+#include "PlayerCharacter.h"
 #include "Components/SphereComponent.h"
 
 AEnemy::AEnemy()
@@ -24,14 +25,37 @@ void AEnemy::BeginPlay()
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (IsAlive && FollowTarget)
+	{
+		float MoveDirection = (FollowTarget->GetActorLocation().X - GetActorLocation().X) > 0.f ? 1.f : -1.f;
+
+		if (CanMove)
+		{
+			FVector WorldDirection = FVector(1.f, 0.f, 0.f);
+			AddMovementInput(WorldDirection, MoveDirection);
+		}
+	}
 }
 
 void AEnemy::DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
 {
+	APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+
+	if (Player)
+	{
+		FollowTarget = Player;
+	}
 }
 
 void AEnemy::DetectorOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int OtherBodyIndex)
 {
+	APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+
+	if (Player)
+	{
+		FollowTarget = NULL;
+	}
 }

dc8f95b53b73feff49630a52478f415ade808bd3
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-10-09 16:22:38 +0700

    update "Enemy Attack State"

--- CrustyPirate/Source/CrustyPirate/Enemy.cpp
+++ CrustyPirate/Source/CrustyPirate/Enemy.cpp
@@ -7,6 +7,7 @@
 #include "Components/SphereComponent.h"
 #include "Components/TextRenderComponent.h"
 #include "PaperZDAnimInstance.h"
+#include "Components/BoxComponent.h"
 
 AEnemy::AEnemy()
 {
@@ -17,6 +18,9 @@ AEnemy::AEnemy()
 
 	HPText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("HPText"));
 	HPText->SetupAttachment(RootComponent);
+
+	AttackCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("AttackCollisionBox"));
+	AttackCollisionBox->SetupAttachment(RootComponent);
 }
 
 void AEnemy::BeginPlay()
@@ -27,13 +31,18 @@ void AEnemy::BeginPlay()
 	PlayerDetectorSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::DetectorOverlapEnd);
 
 	UpdateHP(HitPoint);
+
+	OnAttackOverrideEndDelegate.BindUObject(this, &AEnemy::OnAttackOverrideAnimEnd);
+
+	AttackCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::AttackBoxOverlapBegin);
+	EnableAttackCollisionBox(false);
 }
 
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (IsAlive && FollowTarget)
+	if (IsAlive && FollowTarget && !IsStunned)
 	{
 		float MoveDirection = (FollowTarget->GetActorLocation().X - GetActorLocation().X) > 0.f ? 1.f : -1.f;
 		UpdateDirection(MoveDirection);
@@ -46,7 +55,14 @@ void AEnemy::Tick(float DeltaTime)
 				AddMovementInput(WorldDirection, MoveDirection);
 			}
 		}
-		
+		else
+		{
+			// Attack
+			if (FollowTarget->IsAlive)
+			{
+				Attack();
+			}
+		}
 	}
 }
 
@@ -116,6 +132,8 @@ void AEnemy::TakeDamage(int DamageAmount, float StunDuration)
 {
 	if (!IsAlive) return;
 
+	Stun(StunDuration);
+	
 	UpdateHP(HitPoint - DamageAmount);
 
 	if (HitPoint <= 0)
@@ -126,17 +144,95 @@ void AEnemy::TakeDamage(int DamageAmount, float StunDuration)
 
 		IsAlive = false;
 		CanMove = false;
+		CanAttack = false;
 
 		// Play the die animation
 		GetAnimInstance()->JumpToNode(FName("JumpDie"), FName("CrabbyStateMachine"));
+
+		EnableAttackCollisionBox(false);
 		
 	}
 	else
 	{
-		// Enenmy is alive
-
-		// Play the take hit animation
+		// Enenmy is alive and play the take hit animation
 		GetAnimInstance()->JumpToNode(FName("JumpTakeHit"), FName("CrabbyStateMachine"));
 		
+	}
+}
+
+void AEnemy::Stun(float DurationInSeconds)
+{
+	IsStunned = true;
+
+	bool IsTimerAlreadyActive = GetWorldTimerManager().IsTimerActive(StunTimer);
+	if (IsTimerAlreadyActive) GetWorldTimerManager().ClearTimer(StunTimer);
+	
+	GetWorldTimerManager().SetTimer(StunTimer, this, &AEnemy::OnStunTimerTimeout,
+		1.f, false, DurationInSeconds);
+
+	GetAnimInstance()->StopAllAnimationOverrides();
+
+	EnableAttackCollisionBox(false);
+}
+
+void AEnemy::OnStunTimerTimeout()
+{
+	IsStunned = false;
+}
+
+void AEnemy::Attack()
+{
+	if (IsAlive && CanAttack && !IsStunned)
+	{
+		CanAttack = false;
+		CanMove = false;
+
+		GetAnimInstance()->PlayAnimationOverride(AttackAnimSequence, FName("DefaultSlot"), 1.f,
+			0.f, OnAttackOverrideEndDelegate);
+
+		GetWorldTimerManager().SetTimer(AttackCooldownTimer, this, &AEnemy::OnAttackCooldownTimerTimeout,
+			1.f, false, AttackCooldownInSeconds);
+	}
+}
+
+void AEnemy::OnAttackCooldownTimerTimeout()
+{
+	if (IsAlive)
+	{
+		CanAttack = true;
+	}
+}
+
+void AEnemy::OnAttackOverrideAnimEnd(bool Completed)
+{
+	if (IsAlive)
+	{
+		CanMove = true;
+	}
+}
+
+void AEnemy::AttackBoxOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
+{
+	APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+	
+	if (Player)
+	{
+		// Player->TakeDamage();
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::White, TEXT("Player take Enemy"));
+	}
+}
+
+void AEnemy::EnableAttackCollisionBox(bool Enabled)
+{
+	if (Enabled)
+	{
+		AttackCollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AttackCollisionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	}
+	else
+	{
+		AttackCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		AttackCollisionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
 	}
 }
--- CrustyPirate/Source/CrustyPirate/Enemy.h
+++ CrustyPirate/Source/CrustyPirate/Enemy.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
+#include "PaperZDAnimInstance.h"
 #include "Enemy.generated.h"
 
 /**
@@ -21,9 +22,15 @@ public:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class UTextRenderComponent* HPText;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UBoxComponent* AttackCollisionBox;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class APlayerCharacter* FollowTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	const UPaperZDAnimSequence* AttackAnimSequence;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float StopDistanceToTarget = 70.f;
@@ -31,11 +38,25 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int HitPoint = 100;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AttackCooldownInSeconds = 3.f;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	bool IsAlive = true;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool IsStunned = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	bool CanMove = true;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool CanAttack = true;
+
+	struct FTimerHandle StunTimer;
+
+	struct FTimerHandle AttackCooldownTimer;
+	FZDOnAnimationOverrideEndSignature OnAttackOverrideEndDelegate;
 
 	AEnemy();
 	virtual void BeginPlay() override;
@@ -55,4 +76,18 @@ public:
 	void UpdateHP(int NewHP);
 
 	void TakeDamage(int DamageAmount, float StunDuration);
+
+	void Stun(float DurationInSeconds);
+	void OnStunTimerTimeout();
+
+	void Attack();
+	void OnAttackCooldownTimerTimeout();
+	void OnAttackOverrideAnimEnd(bool Completed);
+
+	UFUNCTION()
+	void AttackBoxOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void EnableAttackCollisionBox(bool Enabled);
 };
